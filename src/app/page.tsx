359c48af9d1444040e8e4fb0cd2d70377135e385
josh <hey@imjosh.dev>
2025-11-17 21:04:11 -0600

    feat: clean up personal profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,12 @@ export default function Page() {
           <a href="https://manifold.inc" className="hover:underline">
             Manifold Labs
           </a>
-          . We design incentive mechanisms for running distributed inference at
-          scale, along with some other cool projects when we are inspired. Some
-          of the projects I{"'"}ve worked on at manifold include{" "}
-          <a href="https://github.com/manifold-inc/targon">Subnet 4</a>,{" "}
-          <a href="https://targon.com/">Targon Hub</a>,{" "}
+          . We are building a compute market with an un-interruptible experience
+          at a interruptible price, along with some other cool projects when we
+          are inspired. Some of the projects I{"'"}ve worked on at manifold
+          include <a href="https://github.com/manifold-inc/targon">Subnet 4</a>,{" "}
+          <a href="https://targon.com/">Targon</a>,{" "}
+          <a href="https://tao.xyz/">Tao xyz</a>,{" "}
           <a href="https://sybil.com/">Sybil</a>, and designing{" "}
           <a href="https://epistula.sybil.com/">the Epistula Protocol</a>.
         </p>
@@ -29,46 +30,15 @@ export default function Page() {
         <p>
           In 2023 I formed my own LLC called{" "}
           <a href="https://brunuslabs.com">Brunus Labs</a> to take on contract
-          work. During my time at Brunus
-          <sup>
-            <a href="#footnote-1" className="pb-1 text-xs no-underline">
-              [1]
-            </a>
-          </sup>{" "}
-          we built internal time tracking applications, lightweight POS systems,
-          discord bots, Marketing sites and more.
+          work. During my time at Brunus we built internal time tracking
+          applications, lightweight POS systems, discord bots, Marketing sites
+          and more.
         </p>
         <p>
           Before Brunus Labs I was a Web and Application Developer at Cleveland
           Clinic. I created multiple web applications and moved all of our teams
           projects from TFVC to git (that wasn{"'"}t fun) amongst other things.
         </p>
-        <p id="footnote-1" className="text-sm">
-          [1]: I was able to hire 3 of my friends under Brunus Labs during
-          college. Matt, Ahmed and Will helped tremendously in not only building
-          Brunus Labs but also helping me become the developer I am today.
-        </p>
-        <h3>Personal Background</h3>
-        <p>
-          Born and raised in Ohio, I always loved computers and technology. I
-          first was introduced to programming in 6th grade where I started
-          writing DOS shell scripts on my schools computers. I got introduced to
-          python the following year where I found my love for programming and
-          solving interesting problems.
-        </p>
-        <div>
-          <p>
-            I went on to attended the{" "}
-            <a href="https://uakron.edu">University of Akron</a>, where I
-            achieved
-          </p>
-          <ul>
-            <li> Bachelors of Applied Mathematics</li>
-            <li> Bachelors of Computer Science</li>
-            <li> Minor in Statistics</li>
-            <li> Minor in Business Administration</li>
-          </ul>
-        </div>
         <h3>What I{"'"}m about</h3>
         <p>
           I love motivating people to build cool stuff, and encourage people to
@@ -83,10 +53,6 @@ export default function Page() {
           <a href="https://github.com/pingdotgg/uploadthing">uploadthing</a>
           {" and "}
           <a href="https://github.com/mdx-editor/editor">MDX Editor</a>.
-        </p>
-        <p>
-          Outside of technology, I really love longboarding, working out, and
-          spending time with my friends & meeting new people along the way.
         </p>
       </div>
     </div>
