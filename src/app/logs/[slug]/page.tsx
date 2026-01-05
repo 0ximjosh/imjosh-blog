95204925eaea7c1b3dc108e150fddcfc97192071
josh <hey@imjosh.dev>
2026-01-04 22:39:11 -0600

    fix: link to next / prev day

--- src/app/logs/[slug]/page.tsx
+++ src/app/logs/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import fsPromises from "fs/promises";
 import matter from "gray-matter";
+import Link from "next/link";
 import path from "path";
 import rehypeHighlight from "rehype-highlight";
 import rehypeSanitize from "rehype-sanitize";
@@ -8,7 +9,7 @@ import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
-import { titleToFilename } from "~/utils/utils";
+import { filenameToTitle, titleToFilename } from "~/utils/utils";
 
 function capitalize(s: string) {
   return s
@@ -41,15 +42,49 @@ const toMarkdown = (md: string) => {
 };
 
 export default async function Page({ params }: { params: { slug: string } }) {
+  const filespath = path.join(process.cwd(), "src", "logs");
+  const posts = await fsPromises.readdir(filespath, { withFileTypes: false });
+  const orderedPosts = posts
+    .map((f) => {
+      return { slug: filenameToTitle(f.slice(0, -3)) };
+    })
+    .sort(
+      (a, b) =>
+        new Date(b.slug ?? "").getTime() - new Date(a.slug ?? "").getTime(),
+    );
+  const idx = orderedPosts.findIndex((v) => v.slug === params.slug);
+
   const filename = titleToFilename(params.slug);
   const filepath = path.join(process.cwd(), "src", "logs", filename + ".md");
   const file = await fsPromises.readFile(filepath, "utf8");
   const data = matter(file);
   return (
     <div className="prose dark:prose-invert">
-      <div className="mb-5">Posted {new Date(params.slug).toLocaleString().split(",")[0]}</div>
+      <div className="mb-5">
+        Posted {new Date(params.slug).toLocaleString().split(",")[0]}
+      </div>
       <div dangerouslySetInnerHTML={{ __html: toMarkdown(data.content) }}></div>
       <hr />
+      <div className="mb-5 flex w-full justify-between text-lg">
+        {idx !== orderedPosts.length - 1 ? (
+          <Link
+            className="no-underline hover:text-orange-600 dark:hover:text-orange-500"
+            href={"/logs/" + orderedPosts[idx + 1]!.slug}
+          >
+            Previous Day
+          </Link>
+        ) : (
+          <div />
+        )}
+        {idx !== 0 && (
+          <Link
+            className="no-underline hover:text-orange-600 dark:hover:text-orange-500"
+            href={"/logs/" + orderedPosts[idx - 1]!.slug}
+          >
+            Next Day
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
